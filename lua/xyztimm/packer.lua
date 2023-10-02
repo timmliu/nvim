4d36163eceb245afcb1e619d99e93284004f58d9
timm <timmcliu@gmail.com>
2023-10-02 02:24:07 -0400

    change search file search mapping

--- lua/xyztimm/packer.lua
+++ lua/xyztimm/packer.lua
@@ -13,8 +13,8 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({ 
-	  'rose-pine/neovim', 
+  use({
+	  'rose-pine/neovim',
 	  as = 'rose-pine',
 	  config = function()
 		  vim.cmd('colorscheme rose-pine')
