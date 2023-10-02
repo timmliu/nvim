961ba7cc2fbc13dedf1c13a85ceb6ad1d9c8369b
timm <timmcliu@gmail.com>
2023-10-02 01:51:00 -0400

    Initial commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,36 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+	ensure_installed = {
+		'tsserver',
+		'eslint',
+		'rust_analyzer',
+	},
+	handlers = {
+		lsp_zero.default_setup,
+	},
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+	['<C-y>'] = cmp.mapping.confirm({ select = true }),
+	['<C-Space>'] = cmp.mapping.complete(),
+})
+
+lsp.set_preferences({
+  sign_icons = { }
+})
---
+++ lua/xyztimm/init.lua
@@ -0,0 +1,3 @@
+require("xyztimm.remap")
+require("xyztimm.packer")
+require("xyztimm.set")
