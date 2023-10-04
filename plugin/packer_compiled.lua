be3064eff0b8e6937fcaeac4dd39f96553b67c13
timm <timm@huckleberry-labs.com>
2023-10-03 22:51:21 -0400

    nvim-comment plugin

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -109,6 +109,11 @@ _G.packer_plugins = {
     path = "/Users/timm/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
+  ["nvim-comment"] = {
+    loaded = true,
+    path = "/Users/timm/.local/share/nvim/site/pack/packer/start/nvim-comment",
+    url = "https://github.com/terrortylor/nvim-comment"
+  },
   ["nvim-lspconfig"] = {
     loaded = true,
     path = "/Users/timm/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
