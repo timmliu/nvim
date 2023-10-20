1ff9efabe7878792e8cd219a45f4922246a52d74
timm <timmcliu@gmail.com>
2023-10-19 23:36:55 -0400

    fw grep current word

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>fh', builtin.find_files, {}) --all files
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {}) --respects gitignore
 vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find, {}) --grep current
 vim.keymap.set('n', '<leader>fs', builtin.live_grep, {}) --grep all working tree live
+vim.keymap.set('n', '<leader>fw', builtin.grep_string, {}) --grep string at cursor
 vim.keymap.set('n', '<leader>gr', function() --grep
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
